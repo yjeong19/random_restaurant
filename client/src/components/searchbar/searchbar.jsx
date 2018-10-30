7a6d31158d720b264fcaf7d725437d01ac72cf41
Young Jeong <yjeong19@gmail.com>
2018-10-29 21:36:27 -0700

    server requests work

--- client/src/components/searchbar/searchbar.jsx
+++ client/src/components/searchbar/searchbar.jsx
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
+const axios = require('axios');
 
 class searchbar extends Component {
   constructor(props){
     super(props);
 
     //var to store searchbar info
-    this.searchbarInput = null;
+    this.searchbarInput = {
+      location: '',
+      categories: '',
+    };
 
     //bind events
     this.handleSearchInput = this.handleSearchInput.bind(this);
   }
 
-  //this makes keystroke in search bar to searchbarInput var
+  //this makes keystroke in search bar to searchbarInput object
   handleSearchInput(e){
-    this.searchbarInput = e.target.value;
-    console.log(this.searchbarInput);
+    switch(e.target.id){
+      case 'categories':
+        //this may be changed to an array of options later.
+        this.searchbarInput.categories = e.target.value;
+        break;
+      case 'location':
+        this.searchbarInput.location = e.target.value;
+        break;
+      default:
+        return;
+    }
+    // this.searchbarInput = e.target.value;
+    // console.log(this.searchbarInput);
+  }
+
+  //submit function --
+  //makes api call to yelpAPI
+  //api returns either random option or a list of options
+  //make one for random and one button for regular search
+
+  //initally making one submit, will do case? or other way to decide which button was chosen;
+  handleSubmit(){
+    axios.get('http://localhost:8081/yelp/random?term&location=san jose&categories=coffee&price=3&limit=10',{
+      headers: {
+	       'Access-Control-Allow-Origin': '*',
+	      },
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
 
@@ -22,8 +57,9 @@ class searchbar extends Component {
   render(){
     return(
       <div>
-        <input onChange = {this.handleSearchInput}></input>
-        <button>submit</button>
+        <input id = 'categories' onChange = {this.handleSearchInput}></input>
+        <input id = 'location' onChange = {this.handleSearchInput}></input>
+        <button onClick = {this.handleSubmit}>submit</button>
       </div>
     )
   }
--- server/server.js
+++ server/server.js
@@ -6,21 +6,20 @@ const PORT = process.env.PORT || 8081;
 // dotenv.config()
 // const dotenv = require('dotenv');
 require('dotenv').config();
-//yelp routes
 const yelp = require('yelp-fusion');
 const yelpAPI = require('./api/yelp');
-//api Routes
 const routes = require('./controller/routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('hello');
-});
-app.use('/', routes);
+//yelp routes
 app.use('/yelp', yelpAPI);
+//api Routes
+app.use('/', routes);
+
+
 
 app.listen(PORT, ()=>{
   console.log(`app listening on port ${PORT}`);
